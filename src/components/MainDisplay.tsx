6119a0c05784a5500d3fce716101d6e7e7e2bf74
Barney Szabolcs <szabolcs.barnabas@gmail.com>
2025-08-06 17:56:41 +0100

    feature: Adds animation when selecting a new day, using framer-motion.

--- src/components/MainDisplay.tsx
+++ src/components/MainDisplay.tsx
@@ -1,13 +1,15 @@
 import type { Location, WeatherData } from '../types/weather'
 import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 
 interface WeatherDisplayProps {
   location: Location;
   weather: WeatherData | null;
   isLoading: boolean;
+  selectedDate: string;
 }
 
-export const MainDisplay: React.FC<WeatherDisplayProps> = ({ location, weather, isLoading }) => {
+export const MainDisplay: React.FC<WeatherDisplayProps> = ({ location, weather, isLoading, selectedDate }) => {
 
   function getLocation () {
     return isLoading ? 'Loading...' : (
@@ -18,35 +20,46 @@ export const MainDisplay: React.FC<WeatherDisplayProps> = ({ location, weather, 
   }
 
   return (
-    <div className="overflow-hidden px-6 flex flex-col gap-4">
-      <div className="font-semibold self-center text-sm mb-2">
-        {getLocation()}
-      </div>
-      <div className="flex items-start justify-between mb-2">
-        <div className="flex flex-col items-center gap-2 pl-6">
-          {weather?.weather_icon ?
-            <img src={weather.weather_icon} alt="" className="w-12 h-12"/> :
-            <div className="w-12 h-12"/>}
-          <div className="text-sm">{weather?.weather_description || '-'}</div>
-        </div>
-        <div className="text-3xl pt-3">{weather ? weather.temperature : '-'}&deg;c</div>
-        <table className="text-xs">
-          <tbody>
-          <tr>
-            <td className="pr-2">Wind:</td>
-            <td>{weather ? weather.wind_speed : '-'} km/h</td>
-          </tr>
-          <tr>
-            <td className="pr-2">Pressure:</td>
-            <td>{weather ? weather.pressure : '-'} hPa</td>
-          </tr>
-          <tr>
-            <td className="pr-2">Precip:</td>
-            <td>{weather ? weather.precip : '-'} mm</td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
+    <div className="overflow-hidden">
+      <AnimatePresence mode="popLayout">
+        <motion.div
+          key={selectedDate}
+          initial={{ opacity: 0, y: 100, scale: 1.1 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          exit={{ opacity: 0, y: -25, scale: 0.95 }}
+          transition={{ duration: 0.5, ease: 'easeInOut' }}
+          className="px-6 flex flex-col gap-4"
+        >
+          <div className="font-semibold self-center text-sm mb-2">
+            {getLocation()}
+          </div>
+          <div className="flex items-start justify-between mb-2">
+            <div className="flex flex-col items-center gap-2 pl-6">
+              {weather?.weather_icon ?
+                <img src={weather.weather_icon} alt="" className="w-12 h-12"/> :
+                <div className="w-12 h-12"/>}
+              <div className="text-sm">{weather?.weather_description || '-'}</div>
+            </div>
+            <div className="text-3xl pt-3">{weather ? weather.temperature : '-'}&deg;c</div>
+            <table className="text-xs">
+              <tbody>
+              <tr>
+                <td className="pr-2">Wind:</td>
+                <td>{weather ? weather.wind_speed : '-'} km/h</td>
+              </tr>
+              <tr>
+                <td className="pr-2">Pressure:</td>
+                <td>{weather ? weather.pressure : '-'} hPa</td>
+              </tr>
+              <tr>
+                <td className="pr-2">Precip:</td>
+                <td>{weather ? weather.precip : '-'} mm</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   )
 }
